a288a0aba73065ee1c966b26d02ef74d57a12220
vlat <anonim270208@gmail.com>
2026-04-13 22:25:02 +0500

    feat: add Header component and implement hover effects for squares

---
+++ app/components/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <div id="header">
+      <h1>CSS Practice</h1>
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,17 @@
-
+import Header from "./components/Header";
 import Square from "./components/Square";
 
 export default function Home() {
   return (
+    <>
+    <Header />
     <div id='container'>
+      
       <Square bgColor="bg-red-200" />
       <Square bgColor="bg-blue-200" />
       <Square bgColor="bg-green-200" />
       <Square bgColor="bg-yellow-200" />
-      </div>
+    </div>
+</>
   );
 }
